24419551a252f417df78ca4cf7bbe90f16a4ff45
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2019-06-05 19:59:47 -0300

    Adiciona cidades favoritas no dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,23 +3,24 @@
 class DashboardController < ApplicationController
 
   before_action :authenticate_user!
-  before_action :search_weather, only: %i[index]
   before_action :favorite_cities_scope, only: %i[index]
   respond_to :html, :js
 
   def index
+    search_weather
   end
 
+
+
+  private
 
   def search_weather(city = params[:city])
     weather_service = WeatherApiService.new(city).request_weather
     @weather = WeatherPresenter.new(weather_service)
   end
 
-  private
-
   def favorite_cities_scope
     @cities = FavoriteCitiesQuery.new(current_user.id).search_names
-    @weather_favorite_cities = FavoriteCitiesQuery.new(current_user.id)
+    @weather_favorite_cities = @cities.map { |city| search_weather(city) }
   end
 end
