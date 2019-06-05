0268dd99e698698954bbdb13f3a34ae1fd9cf2c9
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2019-06-05 18:04:48 -0300

    Altera configurações do rails_helper para ambiente de teste

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+ENV['RAILS_ENV'] ||= 'test'
+
 require 'spec_helper'
 require File.expand_path('../config/environment', __dir__)
 require 'rails/all'
@@ -16,7 +18,6 @@ require 'devise'
 
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |file| require file }
 
-ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
@@ -27,11 +28,16 @@ rescue ActiveRecord::PendingMigrationError => error
   exit 1
 end
 RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
+
   config.use_transactional_fixtures = false
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
   config.order = 'random'
+
+  config.include Devise::Test::ControllerHelpers, type: :controller
+
   Capybara.javascript_driver = :webkit
   Capybara.server = :webrick
 end
--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,24 +1,25 @@
 RSpec.configure do |config|
-  config.add_setting(:seed_tables)
-  config.seed_tables = %w(seed_table_1 seed_table_2)
-
   config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation, except: config.seed_tables)
+    DatabaseCleaner.clean_with(:truncation)
   end
-
   config.before(:each) do
     DatabaseCleaner.strategy = :transaction
   end
-
-  config.before(:each, js: true) do
-    DatabaseCleaner.strategy = :truncation, {except: config.seed_tables}
+  config.before(:each, :js => true) do
+    DatabaseCleaner.strategy = :truncation
   end
-
   config.before(:each) do
     DatabaseCleaner.start
   end
-
   config.after(:each) do
     DatabaseCleaner.clean
   end
+  config.before(:all) do
+    DatabaseCleaner.start
+  end
+  config.after(:all) do
+    DatabaseCleaner.clean
+  end
 end
+
+
