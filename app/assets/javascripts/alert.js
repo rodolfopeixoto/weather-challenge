59aff582f9a41ea54f54b0723b30bc16678c94c9
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2019-06-06 01:02:19 -0300

    Melhora o flash message e adiciona funcão de esconder em 5 segudnos

---
+++ app/assets/javascripts/alert.js
@@ -0,0 +1,5 @@
+$(function() {
+  setTimeout(function() {
+    $('.alert').fadeOut('fast');
+  }, 5000);
+})
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,13 @@ module ApplicationHelper
   def header_application
     render '/shared/header' if current_user
   end
+
+  def flash_class(level)
+    case level
+      when 'notice' then "alert alert-info"
+      when 'success' then "alert alert-success"
+      when 'error' then "alert alert-danger"
+      when 'alert' then "alert alert-warning"
+    end
+  end
 end
