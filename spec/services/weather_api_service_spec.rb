00e3174702b948b0b64a3fc6f57b659dc34763dd
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2019-06-06 04:38:38 -0300

    Adiciona teste ao service weather_api_service

--- spec/services/weather_api_service_spec.rb
+++ spec/services/weather_api_service_spec.rb
@@ -2,9 +2,22 @@
 
 require 'rails_helper'
 
-RSpec.describe WeatherApiService do
-  let!(:api) { WeatherApiService.new('Sao Paulo,BR').request_weather }
-  it '.request_weather', :vcr do
-    expect(api.temperature.to_i).to be_kind_of(Integer)
+RSpec.describe WeatherApiService, :vcr do
+  let!(:api_error_response) { WeatherApiService.new('aaaa').request_weather }
+  let!(:api_error_response_empty) { WeatherApiService.new('').request_weather }
+  let!(:api) { WeatherApiService.new('São Paulo').request_weather }
+  context '.request_weather' do
+    it 'when api connect with success' do
+      expect(api.temperature.to_i).to be_kind_of(Integer)
+    end
+  end
+  context '.check_error_api' do
+    it 'when there is no city' do
+      expect(api_error_response[:message]).to eq('Cidade não encontrada.')
+    end
+
+    it 'when the city field is empty' do
+      expect(api_error_response_empty[:message]).to eq('Cidade não encontrada.')
+    end
   end
 end
